7df1bc0e211f3a990bf4b464f49e718a51999ead
dibaliklayar525 <dibaliklayar525@gmail.com>
2023-12-02 13:30:35 +0700

    crud di request

--- backend/routes/UserRoute.js
+++ backend/routes/UserRoute.js
@@ -1,2 +1,16 @@
 import express from "express";
-import {getUsers} from "../constroller/UserController.js";
+import {
+    getUsers,
+    getUserById,
+    createUser, updateUser, deleteUser
+} from "../constroller/UserController.js";
+
+const router = express.Router();
+
+router.get('/users', getUsers);
+router.get('/users/:id', getUserById);
+router.post('/users', createUser);
+router.patch('/users/:id', updateUser);
+router.delete('/users/:id', deleteUser);
+
+export default router;
